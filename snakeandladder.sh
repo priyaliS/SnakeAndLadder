db07113ad3b2e9623da57eed9aaa9e234d20a4c1
priyalisakhare31@gmail.com <priyalisakhare31@gmail.com>
2020-07-08 09:02:19 +0530

    [Priyali]Added:option to moving position,snake and ladder

--- snakeandladder.sh
+++ snakeandladder.sh
@@ -4,6 +4,30 @@ echo "welcome to snake and ladder simulator"
 
 playeronePosition=0
 
-dieValue=$(( (( $RANDOM%6 ))+1 ))
+function playerMovement()
+{
 
-echo "player one die value is : " $dieValue
+	dieValue=$(( (( $RANDOM%6 ))+1 ))
+
+	echo "player one die value is : " $dieValue
+
+	case $(( (( $RANDOM%3 ))+1 )) in
+
+		1) echo "no play" ;;
+
+		2) playeronePosition=$(( $playeronePosition+$dieValue ))
+		   echo "player gone through ladder" ;;
+
+		3) playeronePosition=$(( $playeronePosition-$dieValue ))
+		   echo "player gone through snake" ;;
+	esac
+
+	if [ $playeronePosition -lt 0 ]
+	then
+		playeronePosition=0
+	fi
+
+	echo "Player position is : "$playeronePosition
+}
+
+playerMovement
